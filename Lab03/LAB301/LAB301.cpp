21b21a84e9421dfc35333bcbe65223c107ae81f2
tony-ja007 <6711800007@mut.ac.th>
2025-12-02 17:55:57 +0700

    Lab03

---
+++ Lab03/LAB301/LAB301.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string studentID;
+    string studentname;
+    float score;
+    string grade;
+
+    cout << "Enter your studentID: ";
+    cin >> studentID;
+    cin.ignore();
+    cout << "Enter your studentname: ";
+    getline(cin, studentname);
+
+    cout << "Enter your score: ";
+    cin >> score;
+    if (score >= 90) {
+        grade = "Grade A";
+    }
+    else if (score >= 80) {
+        grade = "Grade B";
+    }
+    else if (score >= 70) {
+        grade = "Grade C";
+    }
+    else if (score >= 60) {
+        grade = "Grade D";
+    }
+    else {
+        grade = "Grade F";
+    }
+    cout << "\n";
+    cout << "========Student Report========" << endl;
+    cout << "StudentID: " << studentID << endl;
+    cout << "Studentname: " << studentname << endl;
+    cout << "Score: " << score << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "==============================" << endl;
+
+    return 0;
+}
